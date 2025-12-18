88b62b6abce27d7ad0b8e0e94ce10430252f2006
maxie <infomaxieandme@gmail.com>
2025-12-18 21:19:06 +0100

    fix: synced vpc variable names and eks dependencies

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ module "vpc" {
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   azs                   = var.azs
-  eip_id                = var.eip_id
+  # FIX: Rename to eip_ids and wrap in brackets [] to make it a list
+  eip_ids               = [var.eip_id] 
 }
 
 module "iam" {
@@ -16,6 +17,8 @@ module "iam" {
 module "eks" {
   source               = "./modules/eks"
   env                  = var.env
+  # FIX: Pass the VPC ID from the VPC module output
+  vpc_id               = module.vpc.vpc_id 
   private_subnet_ids   = module.vpc.private_subnet_ids
   cluster_role_arn     = module.iam.eks_cluster_role_arn
   node_role_arn        = module.iam.eks_node_role_arn
