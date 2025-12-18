69e968859eae888f32972768c61f4fc83ed9e447
maxie <infomaxieandme@gmail.com>
2025-12-18 19:06:08 +0100

    created a prod/main.tf

---
+++ environments/prod/main.tf
@@ -0,0 +1,16 @@
+module "production_infrastructure" {
+  source = "../../"
+
+  # Pass variables from your prod.tfvars into the root modules
+  env                  = var.env
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                  = var.azs
+  eip_id               = var.eip_id
+  node_desired_size    = var.node_desired_size
+  node_min_size        = var.node_min_size
+  node_max_size        = var.node_max_size
+  instance_types       = var.instance_types
+  ecr_repos            = var.ecr_repos
+}
